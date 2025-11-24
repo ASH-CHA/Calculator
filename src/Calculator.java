1687aff9e4548ac3ed35f804dd3d69c5fff28391
ASH-CHA <ashton15chavez@gmail.com>
2025-11-24 15:06:25 +0100

    Created Frame

--- src/Calculator.java
+++ src/Calculator.java
@@ -18,7 +18,13 @@ public class Calculator implements ActionListener{
     char operator;
 
     Calculator(){
+        frame = new JFrame("Calculator");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(420, 550);
+        frame.setLayout(null);
 
+
+        frame.setVisible(true);
     }
 
     public static void main(String[] args) throws Exception {
